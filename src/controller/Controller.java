54d13472bb2afcae7f09a816f38e1418de14e56b
Baptiste <bababapt@gmail.com>
2017-11-13 13:02:59 +0100

    Update Controller.java

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -5,18 +5,17 @@ import view.Observateur;
 
 public class Controller {
 
-	
 	private Observateur obs;
-    private Jeu jeu;
+    	private Jeu jeu;
     
-    public Controller () {
-    	this.jeu = Jeu.getInstance();
-    }
-    
-    // Méthode MAIN du logiciel.
-    public static void main(String [] args) {
-    	
-    	
-    }
+	public Controller () {
+	this.jeu = Jeu.getInstance();
+	}
+
+	// MÃ©thode MAIN du logiciel.
+	public static void main(String [] args) {
+
+
+	}
 
 }
