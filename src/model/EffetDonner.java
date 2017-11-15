4cc7f0a0a48a0c9ebf0ab8b787767ff90c3b0602
Baptiste <bababapt@gmail.com>
2017-11-15 13:00:48 +0100

    Update EffetDonner.java

--- src/model/EffetDonner.java
+++ src/model/EffetDonner.java
@@ -4,14 +4,9 @@ package model;
 public class EffetDonner implements Effet {
 
     
-	public void action(Joueur JoueurCourant) {
+	public String action(Joueur JoueurCourant) {
 		
     }
 
-	@Override
-	public String getMessage(Joueur joueurCourant) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
