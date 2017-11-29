6ae64c9e0a8de3c9042c56e6f5729827b850ba46
Thomas <Thomas@LAPTOP-IROP17R3>
2017-11-29 17:42:30 +0100

    choisirDataDonner agressif = passif

--- src/model/Passif.java
+++ src/model/Passif.java
@@ -16,9 +16,24 @@ public class Passif implements Strategie {
 
 	@Override
 	public int[] choisirDataDonner(Joueur joueurCourant) {
+		
+		int indexCarteChoisie = 0;
+		for (int i = 1 ; i < joueurCourant.getMain().size() ; i++) {
+			if (joueurCourant.getMain().get(indexCarteChoisie).getEffet() instanceof EffetAttaque || joueurCourant.getMain().get(indexCarteChoisie).getEffet() instanceof EffetContrerChangerCouleur) {
+				indexCarteChoisie = i;
+			}
+		}
+		
+		int indexJoueurChoisi = 0;
+		for (int j = 1 ; j < Jeu.getInstance().getJoueurs().size() - 1 ; j++) { //le joueur courant est en dernière position de cette linkedList normalement
+			if (Jeu.getInstance().getJoueurs().get(indexJoueurChoisi).getMain().size() > Jeu.getInstance().getJoueurs().get(j).getMain().size()) {
+				indexJoueurChoisi = j;
+			}
+		}
+		
 		int [] data = new int [2];
-		data[0] = 0;
-		data[1] = 0;
+		data[0] = indexCarteChoisie;
+		data[1] = indexJoueurChoisi;
 		return data;
 	}
 }
