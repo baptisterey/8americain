ac6e927b4a92f498c8580f029c17d6608e0acdcd
Thomas <Thomas@LAPTOP-IROP17R3>
2018-01-02 18:02:11 +0100

    j'y arrive pas

--- src/view/interfaceGraphique/JoueurArtificiel.java
+++ src/view/interfaceGraphique/JoueurArtificiel.java
@@ -3,6 +3,8 @@ package view.interfaceGraphique;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 
 import javax.swing.ImageIcon;
@@ -13,6 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import controleur.Controleur;
+import model.Joueur;
 import view.IHM;
 
 public class JoueurArtificiel extends IHM {
@@ -23,9 +26,10 @@ public class JoueurArtificiel extends IHM {
 	private JButton button; // pour pouvoir le selectionner (effet donner par exemple)
 	private JCheckBox checkBox; // coché s'il a bien annoncé "carte" (pas besoin de préciser pour les autres annonces ?)
 
-	public JoueurArtificiel(Controleur ctrl, String pathImage, String pseudo, int nombreCarte) {
+	public JoueurArtificiel(Controleur ctrl, String pathImage, Joueur j) {
 		super(ctrl);
-		initialize(pathImage, pseudo, nombreCarte);
+		
+		initialize(pathImage, j);
 		
 		
         
@@ -33,8 +37,9 @@ public class JoueurArtificiel extends IHM {
 		// TODO Auto-generated constructor stub
 	}
 	
-	private void initialize(String pathImage, String pseudo, int nombreCarte) {
-		this.fenetre = new JFrame();
+	private void initialize(String pathImage, Joueur j ) {
+		this.fenetre = new JFrame();//faut il donner une taille ? le rendre visible ?
+	
 		JPanel panel = new JPanel(new BorderLayout());
 		Container reservoir = fenetre.getContentPane();
 		reservoir.add(panel);
@@ -44,6 +49,9 @@ public class JoueurArtificiel extends IHM {
         panel.add(this.image);//marche pas parce qu'il faut un component............
         //
         
+        String pseudo = j.getPseudo();
+        int nombreCarte = j.getMain().size();
+        
         //North
         this.texte = new JLabel(pseudo + " : "+nombreCarte+" cartes");
         panel.add(texte, BorderLayout.NORTH);
@@ -51,15 +59,22 @@ public class JoueurArtificiel extends IHM {
         
         //South
         this.button = new JButton("selectionner");
-       // button.addActionListener(new ActionListener() {
+        checkBox.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
         	
-        //});
+        });
         panel.add(button, BorderLayout.SOUTH);
         //
         
         //West
         this.checkBox = new JCheckBox("a annoncé 'Carte'");
         panel.add(checkBox, BorderLayout.WEST);
+       
         //
 	}
 	
