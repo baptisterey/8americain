d03c06c4722f7b1c119576c0ca600bc622d1d60f
Thomas <Thomas@192.168.1.23>
2018-01-02 11:45:37 +0100

    Partie visuelle du joueur artificiel presque fini, borderlayout ne veut pas d'IconImage...

--- src/view/interfaceGraphique/JoueurArtificiel.java
+++ src/view/interfaceGraphique/JoueurArtificiel.java
@@ -10,7 +10,7 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
+import javax.swing.JPanel;
 
 import controleur.Controleur;
 import view.IHM;
@@ -19,25 +19,50 @@ public class JoueurArtificiel extends IHM {
 	
 	private JFrame fenetre; // j'arrive pas à utiliser borderlayout sans avoir de fenetre --'
 	private ImageIcon image; //pour image du personnage
-	// text mis dans le nom de la fenêtre du coup
-	//private JLabel texte; //pour son pseudo et son nombre de carte
+	private JLabel texte; //pour son pseudo et son nombre de carte
 	private JButton button; // pour pouvoir le selectionner (effet donner par exemple)
 	private JCheckBox checkBox; // coché s'il a bien annoncé "carte" (pas besoin de préciser pour les autres annonces ?)
 
 	public JoueurArtificiel(Controleur ctrl, String pathImage, String pseudo, int nombreCarte) {
 		super(ctrl);
-		this.fenetre = new JFrame(pseudo + " : "+nombreCarte+" cartes");
-        this.image = new ImageIcon(pathImage);
-        //this.texte = new JLabel(pseudo + " : "+nombreCarte+" cartes");
-        this.button = new JButton("selectionner");
-        this.checkBox = new JCheckBox("a annoncé 'Carte'");
-        
-        Container reservoir = fenetre.getContentPane();
-        //reservoir.add(this.image, BorderLayout.CENTER); marche pas je ne sais pas pourquoi
+		initialize(pathImage, pseudo, nombreCarte);
 		
+		
+        
+        
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	private void initialize(String pathImage, String pseudo, int nombreCarte) {
+		this.fenetre = new JFrame();
+		JPanel panel = new JPanel(new BorderLayout());
+		Container reservoir = fenetre.getContentPane();
+		reservoir.add(panel);
+		
+		//Center
+        this.image = new ImageIcon(pathImage);
+        panel.add(this.image);//marche pas parce qu'il faut un component............
+        //
+        
+        //North
+        this.texte = new JLabel(pseudo + " : "+nombreCarte+" cartes");
+        panel.add(texte, BorderLayout.NORTH);
+        //
+        
+        //South
+        this.button = new JButton("selectionner");
+       // button.addActionListener(new ActionListener() {
+        	
+        //});
+        panel.add(button, BorderLayout.SOUTH);
+        //
+        
+        //West
+        this.checkBox = new JCheckBox("a annoncé 'Carte'");
+        panel.add(checkBox, BorderLayout.WEST);
+        //
+	}
+	
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
